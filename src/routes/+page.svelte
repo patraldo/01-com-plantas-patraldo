d63c5148d29eb5b1c17469ee3392ce0c9daa53d5
Chef Tech <cheftech@patraldo.com>
2024-03-17 14:02:46 -0600

    customization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,78 @@
+<script>
+    import { Body } from 'svelte-body';
+    const style = {
+        color: 'green',
+        '--cool-css-prop': '😎'
+    };
+
+    import { page } from '$app/stores';
+    let path;
+
+    $: path = $page.url.pathname;
+
+</script>
+
+
+<Body {style} />
+
+<div class='plantas'> 
+  <h1 style="font-size:11vw">¡Plantas Patraldo!</h1>
+<p>La Plantera Rosa</p>
+</div>
+
 <div class='email'>
 <h1 style="font-size:7vw;"><a href = "mailto:infoplantas@patraldo.com">Email</a></h1>
 </div>
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3732.722775744146!2d-103.3696053!3d20.6808546!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428af2f46b0c6db%3A0x5b98c004442dc9b8!2sLa%20Musa%20que%20M%C3%A1s%20Aplauda!5e0!3m2!1sen!2smx!4v1690053954548!5m2!1sen!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe width="600" height="450" style="border:0" Loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJO-etgvyvKIQRbqwoDvNUPJA&key=AIzaSyDSFnLS3cxT4yHYxcKqf5pXxg6C_OCoVH0"></iframe>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
 
 
+@keyframes move-bg {
+  to {
+    background-position: var(--bg-size) 0;
+  }
+}
 
-@import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
+/* https://web.dev/speedy-css-tip-animated-gradient-text/ */
+.plantas {
+  --bg-size: 400%;
+  --color-one: hsl(7 85% 43%);
+  --color-two: hsl(50 95% 73%);
+  --color-three: hsl(318 94% 7%);
+  background-clip: padding-box;
+font-family: 'Special Elite';
+margin: 10px;
+padding: 10px;
+text-align: center;
+
+background: linear-gradient(
+                90deg,
+                var(--color-one),
+                var(--color-two),
+                var(--color-three)
+              ) 0 0 / var(--bg-size) 100%;
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  animation: move-bg 8s infinite linear;
+}
+
+.plantas p {
+  --bg-size: 200%;
+  --color-one: hsl(7 85% 43%);
+  --color-two: hsl(50 95% 73%);
+  --color-three: hsl(318 94% 7%);
+  background-clip: padding-box;
+font-family: 'Special Elite';
+margin: 10px;
+padding: 10px;
+text-align: center;
+}
+
+
 
 iframe {
 width: 100%;
